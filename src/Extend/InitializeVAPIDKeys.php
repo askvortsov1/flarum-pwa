2cd2c4f88cece336421eb88d6ec105824b0ef86a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-30 19:43:34 -0400

    Add script to initialize VAPID keys on start

---
+++ src/Extend/InitializeVAPIDKeys.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Askvortsov\FlarumPWA\Extend;
+
+use Flarum\Extend\ExtenderInterface;
+use Flarum\Extension\Extension;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Container\Container;
+use Minishlink\WebPush\VAPID;
+
+class InitializeVAPIDKeys implements ExtenderInterface
+{
+
+    public function extend(Container $container, Extension $extension = null)
+    {
+        if (!class_exists(VAPID::class) || !function_exists('gmp_init')) return;
+
+        $settings = $container->make(SettingsRepositoryInterface::class);
+
+        if (!$settings->get('askvortsov-pwa.vapid.private') || !$settings->get('askvortsov-pwa.vapid.private')) {
+            $keys = VAPID::createVapidKeys();
+
+            $settings->set('askvortsov-pwa.vapid.private', $keys['privateKey']);
+            $settings->set('askvortsov-pwa.vapid.public', $keys['publicKey']);
+        }
+    }
+}
