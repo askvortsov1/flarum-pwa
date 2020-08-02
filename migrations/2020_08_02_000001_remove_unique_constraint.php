e7ffc3a4913455ba2ec7275164b7c146901fbbd3
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-08-02 15:09:26 -0400

    Add migration to drop unique constraint on endpoint

---
+++ migrations/2020_08_02_000001_remove_unique_constraint.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of askvortsov/flarum-pwa
+ *
+ *  Copyright (c) 2020 Alexander Skvortsov.
+ *
+ *  For detailed copyright and license information, please view the
+ *  LICENSE file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('push_subscriptions', function (Blueprint $table) {
+            $table->dropUnique(['endpoint']);
+        });
+    },
+    'down' => function (Builder $schema) {
+    },
+];
