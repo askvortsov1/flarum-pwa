8a2d74492e29c3ac5ee10c4ec5e1d9b37de46893
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 02:14:10 -0500

    Admin UX redesign support

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,24 +1,5 @@
-import { extend } from "flarum/extend";
-import AdminNav from "flarum/components/AdminNav";
-import AdminLinkButton from "flarum/components/AdminLinkButton";
-
 import PWAPage from "./components/PWAPage";
 
 app.initializers.add("askvortsov/flarum-pwa", () => {
-  app.routes["askvortsov-pwa"] = { path: "/pwa", component: PWAPage };
-
-  //app.extensionSettings["askvortsov-pwa"] = () => m.route.set(app.route("askvortsov-pwa"));
-
-  extend(AdminNav.prototype, "items", (items) => {
-    items.add(
-      "pwa",
-      <AdminLinkButton
-        href={app.route("askvortsov-pwa")}
-        icon="fas fa-mobile-alt"
-        description={app.translator.trans("askvortsov-pwa.admin.nav.pwa_text")}
-      >
-        {app.translator.trans("askvortsov-pwa.admin.nav.pwa_button")}
-      </AdminLinkButton>
-    );
-  });
+  app.extensionData.for("askvortsov-pwa").registerPage(PWAPage);
 });
