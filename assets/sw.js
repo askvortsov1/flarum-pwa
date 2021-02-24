ac82b14811fdb7478913d1b65220a14898ffec82
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-02-24 10:17:21 -0500

    Don't cache if clockwork enabled

--- assets/sw.js
+++ assets/sw.js
@@ -33,19 +33,19 @@ const offlineFallbackPage = "/offline";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
-    console.log("[PWA] Install event processing...");
+  console.log("[PWA] Install event processing...");
 
-    event.waitUntil(
-        caches.open(CACHE).then(function (cache) {
-            console.log("[PWA] Cached offline page during install.");
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA] Cached offline page during install.");
 
-            if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-                return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-            }
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
 
-            return cache.add(offlineFallbackPage);
-        })
-    );
+      return cache.add(offlineFallbackPage);
+    })
+  );
 
   const receiveInfo = async () => {
     const payload = await idbKeyval.get('flarum.forumPayload');
@@ -57,36 +57,37 @@ self.addEventListener("install", function (event) {
 
 // If any fetch fails, it will show the offline page.
 self.addEventListener("fetch", function (event) {
-    if (event.request.method !== "GET") return;
+  if (event.request.method !== "GET") return;
+  if (forumPayload.debug && forumPayload.clockworkEnabled) return;
 
-    event.respondWith(
-        fetch(event.request).catch(function (error) {
-            // The following validates that the request was for a navigation to a new document
-            if (
-                event.request.destination !== "document" ||
-                event.request.mode !== "navigate"
-            ) {
-                return;
-            }
+  event.respondWith(
+    fetch(event.request).catch(function (error) {
+          // The following validates that the request was for a navigation to a new document
+          if (
+              event.request.destination !== "document" ||
+              event.request.mode !== "navigate"
+          ) {
+              return;
+          }
 
-            console.error("[PWA] Network request failed. Serving offline page " + error);
-            return caches.open(CACHE).then(function (cache) {
-                return cache.match(offlineFallbackPage);
-            });
-        })
-    );
+          console.error("[PWA] Network request failed. Serving offline page " + error);
+          return caches.open(CACHE).then(function (cache) {
+              return cache.match(offlineFallbackPage);
+          });
+      })
+  );
 });
 
 // This is an event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener("refreshOffline", function () {
-    const offlinePageRequest = new Request(offlineFallbackPage);
+  const offlinePageRequest = new Request(offlineFallbackPage);
 
-    return fetch(offlineFallbackPage).then(function (response) {
-        return caches.open(CACHE).then(function (cache) {
-            console.log("[PWA] Offline page updated from refreshOffline event: " + response.url);
-            return cache.put(offlinePageRequest, response);
-        });
+  return fetch(offlineFallbackPage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("[PWA] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
     });
+  });
 });
 
 self.addEventListener('push', function (event) {
