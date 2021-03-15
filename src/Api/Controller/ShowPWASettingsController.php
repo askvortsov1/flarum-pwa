6c68664646560e064cc92258cde9ebb83e3ad9fd
Alexander Skvortsov <askvortsov1@users.noreply.github.com>
2021-03-15 17:13:35 +0000

    Apply fixes from StyleCI

--- src/Api/Controller/ShowPWASettingsController.php
+++ src/Api/Controller/ShowPWASettingsController.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of askvortsov/flarum-pwa
  *
- *  Copyright (c) 2020 Alexander Skvortsov.
+ *  Copyright (c) 2021 Alexander Skvortsov.
  *
  *  For detailed copyright and license information, please view the
  *  LICENSE file that was distributed with this source code.
