f01e67afccde24be1a94349c5061d58c331f27ad
Jaggy <jaggy@hey.com>
2024-04-04 10:40:03 +0800

    CS fixes

--- src/FlarumPWAServiceProvider.php
+++ src/FlarumPWAServiceProvider.php
@@ -8,6 +8,7 @@
  *  For detailed copyright and license information, please view the
  *  LICENSE file that was distributed with this source code.
  */
+
 namespace Askvortsov\FlarumPWA;
 
 use Flarum\Foundation\AbstractServiceProvider;
