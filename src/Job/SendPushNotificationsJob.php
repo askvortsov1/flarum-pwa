7f312cde56006bdde758f50e79ceaef82a145b3a
Jaggy <jaggy@hey.com>
2024-06-16 01:30:03 +0800

    Downgrade Firebase (#59)
    * Use a lower firebase package verison
    
    * Prevent firebase from sending when no firebase settings has been set
    
    * Don't do anything if the settings is empty
    
    * Remove debug
    
    * Add better exception handling
    
    * Fix error handling
    
    * Remove expired token
    
    * CS fixes
    
    * Clean up logging
    
    * Improve json validation
    
    * Prettier
    
    * CS fixes

--- src/Job/SendPushNotificationsJob.php
+++ src/Job/SendPushNotificationsJob.php
@@ -11,13 +11,11 @@
 
 namespace Askvortsov\FlarumPWA\Job;
 
-use Askvortsov\FlarumPWA\FirebaseConfigInvalid;
 use Askvortsov\FlarumPWA\FirebasePushSender;
 use Askvortsov\FlarumPWA\PushSender;
 use ErrorException;
 use Flarum\Notification\Blueprint\BlueprintInterface;
 use Flarum\Queue\AbstractJob;
-use Flarum\Settings\SettingsRepositoryInterface;
 
 class SendPushNotificationsJob extends AbstractJob
 {
@@ -37,22 +35,10 @@ class SendPushNotificationsJob extends AbstractJob
     /**
      * @throws ErrorException
      */
-    public function handle(PushSender $native, FirebasePushSender $firebase, SettingsRepositoryInterface $settings): void
+    public function handle(PushSender $native, FirebasePushSender $firebase): void
     {
         $native->notify($this->blueprint, $this->recipientIds);
 
-        try {
-            $firebase->notify($this->blueprint, $this->recipientIds);
-        } catch (FirebaseConfigInvalid) {
-        }
-    }
-
-    private function hasValidFirebaseSettings(SettingsRepositoryInterface $settings): bool
-    {
-        try {
-            return (bool) json_encode($settings->get('askvortsov-pwa.firebaseConfig'));
-        } catch (\Throwable) {
-            return false;
-        }
+        $firebase->notify($this->blueprint, $this->recipientIds);
     }
 }
